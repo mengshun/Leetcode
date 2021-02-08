0bb77fd1ff010fbdfc074a1c553247631866f221
shunzi <892445213@qq.com>
2021-02-08 18:27:00 +0800

    复习

--- problems/665.py
+++ problems/665.py
@@ -51,3 +51,43 @@ def jiyi(nums):
             else:
                 nums[i] = nums[i-1]
     return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 非递减
+def fuxi(nums):
+    n = len(nums)
+    count = 0
+    for i in range(1, n):
+        if nums[i] < nums[i-1]:
+            count += 1
+            if count > 1:
+                return False
+            if i == 1 or nums[i-2] <= nums[i]:
+                nums[i-1] = nums[i]
+            else:
+                nums[i] = nums[i-1]
+    return True
+
+
+
+print(fuxi([3, 4, 2, 3]))
+
+
+
