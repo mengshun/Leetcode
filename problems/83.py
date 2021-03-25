d8a584c0c9aae789a3a6046dc51495cb7462fb4e
shunzi <892445213@qq.com>
2021-03-25 19:08:21 +0800

    删除链表重复元素

--- problems/83.py
+++ problems/83.py
@@ -1,12 +1,9 @@
 """
 83 删除排序链表中的重复元素
 """
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-def deleteDuplicates(head):
-
+from TreeNode import ListNode
+print("83. 删除排序链表中的重复元素")
+def deleteDuplicatesOne(head):
     dp = []
     last = ListNode(0)
     pre = head
@@ -17,20 +14,109 @@ def deleteDuplicates(head):
             last = pre
         pre = pre.next
         last.next = None
-
     return head
 
-node1 = ListNode(1)
-node2 = ListNode(1)
-node3 = ListNode(2)
-node4 = ListNode(3)
-node5 = ListNode(3)
-node1.next = node2
-node2.next = node3
-node3.next = node4
-node4.next = node5
-deleteDuplicates(node1)
+def deleteDuplicatesOneOther(head):
+    if not head:
+        return None
+    pre = head
+    current = pre.next
+    while current:
+        while current and current.val == pre.val:
+            current = current.next
+        pre.next = current
+        pre = current
+    return head
 
-while node1:
-    print(node1.val)
-    node1 = node1.next
+
+node = ListNode(0).initListNode([1,1,2,3,3])
+deleteDuplicatesOne(node)
+node.printNode()
+
+node = ListNode(0).initListNode([1,1,2,3,3])
+deleteDuplicatesOneOther(node)
+node.printNode()
+
+
+"""
+82. 删除排序链表中的重复元素 II
+https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list-ii/
+"""
+print("\n82. 删除排序链表中的重复元素 II")
+
+"""
+1.  头节点存在和后面节点重复的可能性, 需要人为创建一个头节点
+2.  比较第一个节点和后面节点值是否一样, 直到找到不一样的值为止, 
+    搜索结束, 在搜索的过程中发现存在重复节点, 则直接将头节点和当前的相连接,
+    不存在重复节点, 则移动头节点
+"""
+def deleteDuplicatesTwo(head):
+    # 迭代
+    if not head or not head.next:
+        return head
+
+    hair = ListNode(0)
+    hair.next = head
+
+    pre = hair
+    current = pre.next
+    while current:
+        while current.next and current.val == current.next.val:
+            current = current.next
+        if pre.next == current:
+            pre = pre.next
+        else:
+            pre.next = current.next
+        current = current.next
+    return hair.next
+
+    # # 递归
+    # if not head or not head.next:
+    #     return head
+    # if head.val != head.next.val:
+    #     head.next = deleteDuplicatesTwo(head.next)
+    # else:
+    #     move = head.next
+    #     while move and move.val == head.val:
+    #         move = move.next
+    #     return deleteDuplicatesTwo(move)
+    # return head
+
+    # if not head or not head.next:
+    #     return head
+    #
+    # hair = ListNode(0)
+    # hair.next = head
+    #
+    # prepre = hair
+    # pre = head
+    # current = pre.next
+    # while current:
+    #     should_skip = False
+    #     while current and current.val == pre.val:
+    #         should_skip = True
+    #         current = current.next
+    #     if should_skip:
+    #         prepre.next = current
+    #     else:
+    #         prepre = pre
+    #     if current:
+    #         pre = current
+    #         current = current.next
+    # return hair.next
+
+
+
+
+node = ListNode(0).initListNode([1,1,2,3,3])
+node = deleteDuplicatesTwo(node)
+node.printNode()
+node = ListNode(0).initListNode([1,1,1,2,3])
+node = deleteDuplicatesTwo(node)
+node.printNode()
+node = ListNode(0).initListNode([1,2,3,3,4,4,5])
+node = deleteDuplicatesTwo(node)
+node.printNode()
+node = ListNode(0).initListNode([1,2,3,3])
+node = deleteDuplicatesTwo(node)
+node.printNode()
